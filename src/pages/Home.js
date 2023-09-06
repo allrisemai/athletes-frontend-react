b2f7272dd5acc076f9230260c6b4cdca552c5427
Mai Yossawan <yossawan.nge@vsni.co.uk>
2023-09-06 14:17:45 +0700

    update responsive layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import AlthletItem from "../components/AlthletItem";
-import PlayerItem from "../components/PlayerItem";
+import PlayerItem from "../components/BasPlayerItem";
 import { athletList, playerList } from "../constants/contentList";
 import { imageList } from "../constants/imageList";
 import "../styles/Home.scss";
@@ -7,43 +7,43 @@ import "../styles/Home.scss";
 function Home() {
   return (
     <div className="home">
+      {/* ALTHLETS */}
       <div className="althlets">
-        <div className="althlets__image">
-          <img src={imageList.football_player} alt="football_player" />
+        <div className="althlets__image" />
+        <div className="group">
+          <div className="title-header">
+            <div className="space" />
+            <h1 className="title-header__althlets">Althlets</h1>
+          </div>
+          {athletList.map((item) => (
+            <AlthletItem
+              title={item.title}
+              description={item.description}
+              backgroundColor={item.backgroundColor}
+              textColor={item.textColor}
+              id={item.id}
+            />
+          ))}
         </div>
-
-        <div className="title-header">
-          <div className="space" />
-          <h1 className="title-header__althlets">Althlets</h1>
-        </div>
-        {athletList.map((item) => (
-          <AlthletItem
-            title={item.title}
-            description={item.description}
-            backgroundColor={item.backgroundColor}
-            textColor={item.textColor}
-            id={item.id}
-          />
-        ))}
       </div>
-      {/* PLAYER */}
+      {/* PLAYERS */}
       <div className="player">
-        <div className="player__image">
-          <img src={imageList.basketball_player} alt="basketball_player" />
+        <div className="player__image" />
+        <div className="group">
+          <div className="title-header">
+            <h1 className="title-header__player">Players</h1>
+            <div className="space" />
+          </div>
+          {playerList.map((item) => (
+            <PlayerItem
+              title={item.title}
+              description={item.description}
+              backgroundColor={item.backgroundColor}
+              textColor={item.textColor}
+              id={item.id}
+            />
+          ))}
         </div>
-        <div className="title-header">
-          <h1 className="title-header__player">Players</h1>
-          <div className="space" />
-        </div>
-        {playerList.map((item) => (
-          <PlayerItem
-            title={item.title}
-            description={item.description}
-            backgroundColor={item.backgroundColor}
-            textColor={item.textColor}
-            id={item.id}
-          />
-        ))}
       </div>
     </div>
   );
